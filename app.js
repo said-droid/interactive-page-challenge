2e300e66ff95c0b73b47a45d4308be557ca160fc
Erick Medina <erickmedinabe@gmail.com>
2020-05-29 12:30:31 +0200

    working on tabs

--- app.js
+++ app.js
@@ -15,13 +15,8 @@ function carousel(element) {
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("dot");
 
-    if (element > slides.length) {
-        slideIndex = 1
-    }
-
-    if (element < 1) {
-        slideIndex = slides.length
-    }
+    if (element > slides.length) slideIndex = 1;
+    if (element < 1) slideIndex = slides.length;
 
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
@@ -33,8 +28,25 @@ function carousel(element) {
 
     slides[slideIndex-1].style.display = 'block';
     dots[slideIndex-1].className += ' active';
-
 }
 
+/****************
+ * Tabs
+ * **************/
 
+function openTabs(event, roomNumber) {
+    let i, tabContent, tabLinks;
 
+    tabContent = document.getElementsByClassName('tabContent');
+
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+    tabLinks = document.getElementsByClassName('tablinks');
+    for (i = 0; i < tabLinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(roomNumber).style.display = 'block';
+    event.currentTarget.className += ' active';
+
+}
